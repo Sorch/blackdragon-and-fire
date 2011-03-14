31c38b6c3f6e6debfd1c23b2d2df66f4ae61c132
woloinme@gmail.com <woloinme@gmail.com@a166780d-f826-14ac-9876-69c398c3e534>
2011-03-14 00:24:46 +0000

    updated a few lines cleaned the parser

--- theholder/posts.php
+++ theholder/posts.php
@@ -4,7 +4,7 @@
 // (c) 2010 - 2011 BlackDragon & Fire CMS  Development Team   /
 //  THIS SOFTWARE IS BETA                                     /
 //                                                            /
-// http://code.google.com/p/blackdragon-and-fire              /
+/// http://code.google.com/p/blackdragon-and-fire             /
 ///////////////////////////////////////////////////////////////
 
 
